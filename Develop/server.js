b485a852d49223d411b5ab99416817c7b96db347
Paul Nixon <paulnxn1@gmail.com>
2023-06-02 22:03:50 -0400

    added code notation

---
+++ Develop/server.js
@@ -0,0 +1,69 @@
+// route assignment / port assignment 
+
+const express = require('express');
+const path = require('path');
+const PORT = 4444;
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+
+// path / route to notes.html
+
+app.get('/notes', (req, res) => {
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+}); 
+
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf-8', (error,notes) =>{
+        
+        if (error) {
+            res.json(error);
+        } else {
+            res.json(JSON.parse(notes));
+        }
+    } );
+
+});
+
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    fs.readFile(path.join(__dirname,'/db/db.json'), 'utf-8', (error, notes) => {
+
+        if (error) {
+            res.json(error);
+        } else {
+            notes = JSON.parse(notes);
+            req.body.id = uuid(),
+            notes.push(req.body);
+            fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), () => {
+                res.send('Neato');
+            } )
+        }
+    } )
+})
+
+
+
+// Default to homepage
+app.get('*', (req, res) => {
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+}); 
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log('It works!')}
+    );
