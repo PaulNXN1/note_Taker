08534e74774b54a27f76a0a481c5104851ff159d
Paul Nixon <paulnxn1@gmail.com>
2023-06-05 18:23:07 -0400

    Serve.js update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const path = require('path');
-const PORT = 4444;
+const PORT = process.env.PORT || 4444;
 const fs = require('fs');
 
 // NPM installed for created unique ID
